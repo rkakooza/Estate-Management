22fff878f68c3dded07e653034abb56d453789b5
rkakooza <58144122+rkakooza@users.noreply.github.com>
2026-01-01 13:46:52 -0500

    Configure DATABASE_URL for Render Postgres

--- estate_project/settings.py
+++ estate_project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,7 +159,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # LOGIN_URL = "/admin/login/?next=/dashboard/"
 # LOGIN_REDIRECT_URL = "/dashboard/"
