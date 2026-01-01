42e453b063d3bf15a9b13933248ef6f04a92b9c4
rkakooza <58144122+rkakooza@users.noreply.github.com>
2025-12-31 19:22:53 -0500

    Enable Whitenoise for production static files

--- estate_project/settings.py
+++ estate_project/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%+4j$fq@a!o=^#fjz7^1upo4(h-f%-t8wpxpbbu=4e!()+p!k='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
