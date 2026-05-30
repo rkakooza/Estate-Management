1795f2badb0ca2097698bd62a3035c96d1d2cef4
rkakooza <58144122+rkakooza@users.noreply.github.com>
2026-05-30 15:34:46 -0400

    Use non-manifest static storage for Vercel

--- estate_project/settings.py
+++ estate_project/settings.py
@@ -158,7 +158,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # LOGIN_URL = "/admin/login/?next=/dashboard/"
 # LOGIN_REDIRECT_URL = "/dashboard/"
